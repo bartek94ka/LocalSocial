67ac57c1ada7cc6a7ce0e4935602b22e8c747cbb
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-10-24 21:49:30 +0200

    first commit

---
+++ LocalSocial/wwwroot/Controllers/postController.js
@@ -0,0 +1,10 @@
+﻿var PostsController = function ($scope, $http) {
+    $http.get('api/post')
+        .success(function (data, status, headers, config) {
+            $scope.postsList = data;
+        })
+        .error(function (data, status, headers, config) {
+            //log error
+        });
+
+}
---
+++ LocalSocial/wwwroot/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+    $scope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
+    var list = [];
+    for (var i = 0; i < 100; i++) {
+        list.push({
+            name: 'List Item ' + i,
+            idx: i
+        });
+    }
+    $scope.list = list;
+}]);
