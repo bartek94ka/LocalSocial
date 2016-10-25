a083c8d49cbd3831b8ee307ac7255cfcb244e1cf
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-10-25 18:23:59 +0200

    add sample md-card and sidenav

--- LocalSocial/wwwroot/app.js
+++ LocalSocial/wwwroot/app.js
@@ -14,3 +14,16 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav)
     }
     $scope.list = list;
 }]);
+
+app
+  .module('sidenavDemo2', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+      $scope.toggleLeft = buildToggler('left');
+      $scope.toggleRight = buildToggler('right');
+
+      function buildToggler(componentId) {
+          return function () {
+              $mdSidenav(componentId).toggle();
+          }
+      }
+  });
