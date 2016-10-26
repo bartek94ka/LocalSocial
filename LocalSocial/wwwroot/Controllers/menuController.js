86622e2b8aa2f2079dc6d4e59ef41cfaf3cb54e9
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-10-26 17:28:34 +0200

    sidenav buttons with icons added

--- LocalSocial/wwwroot/Controllers/menuController.js
+++ LocalSocial/wwwroot/Controllers/menuController.js
@@ -1,14 +1,17 @@
 ﻿var MenuController = function () {
     this.items = [];
-    this.items.push(MenuItem("Strona główna", "/"));
-    this.items.push(MenuItem("Znajomi", "/friends"));
-    this.items.push(MenuItem("Moje posty", "/myposts"));
-    this.items.push(MenuItem("Zmiana zasięgu", "/range"));
-    this.items.push(MenuItem("Ustawienia konta", "/settings"));
+    this.items.push(MenuItem("Strona główna", "/", "home"));
+    this.items.push(MenuItem("Znajomi", "/friends", "group"));
+    this.items.push(MenuItem("Moje posty", "/myposts", "note"));
+    this.items.push(MenuItem("Dodaj post", "/addpost", "note_add"));
+    this.items.push(MenuItem("Zmiana zasięgu", "/range", "gps_fixed"));
+    this.items.push(MenuItem("Ustawienia konta", "/settings", "settings"));
+    this.items.push(MenuItem("Wyloguj się", "/", "power_settings_new"));
 };
-MenuItem = function (text, link) {
+MenuItem = function (text, link, icon) {
     return {
         LinkText: text,
         LinkReference: link,
+        LinkIcon: icon,
     };
 }
