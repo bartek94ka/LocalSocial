1fb2c96016935a43a37c5f3740dab31aef3c9a09
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-10-28 00:14:36 +0200

    change .net framework version, add first models

---
+++ LocalSocial/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalSocial.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            //this.Tags = new HashSet<Tag>();
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string PostContent { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime AddDate { get; set; }
+        public string UserId { get; set; }
+        public int LocationId { get; set; }
+        public string Tag { get; set; }
+        public int TagId { get; set; }
+    }
+    public class PostsBindingModel
+    {
+        [Required]
+        public string PostContent { get; set; }
+        public int TagId { get; set; }
+    }
+}
---
+++ LocalSocial/Models/User.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+
+namespace LocalSocial.Models
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int LocationId { get; set; }
+
+        public virtual ICollection<Post> Posts { get; private set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
