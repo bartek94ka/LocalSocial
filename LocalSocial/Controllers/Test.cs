6c0c420842b69fb0768ee4a909ca5d9f726fa23b
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-10-28 15:46:54 +0200

    model extended, create account controller

---
+++ LocalSocial/Controllers/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+using LocalSocial.Models;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity; // Maybe this one too
+
+namespace LocalSocial.Controllers
+{
+
+
+    
+    public class Test
+    {
+        public void aaa()
+        { 
+            //var manager = HttpContext.Current.GetOwinContext().GetUserManager<UserManager<User>>();
+        }
+    }
+}
---
+++ LocalSocial/LocalSocialContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LocalSocial.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace LocalSocial
+{
+    public class LocalSocialContext : IdentityDbContext
+    {
+        // Your context has been configured to use a 'LocalSocialContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'LocalSocial.LocalSocialContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'LocalSocialContext' 
+        // connection string in the application configuration file.
+        public LocalSocialContext()
+            : base("name=Model1")
+        {
+        }
+        public static LocalSocialContext Create()
+        {
+            return new LocalSocialContext();
+        }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
