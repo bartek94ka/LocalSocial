7f21bc9b5ac0004cfe5aecc0efcdf012654f8119
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2016-11-13 13:34:31 +0100

    view modification

---
+++ LocalSocial/wwwroot/Controllers/sideNavController.js
@@ -0,0 +1,15 @@
+﻿var SideNavController = function($scope, $mdSidenav) {
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
+    var list = [];
+    for (var i = 0; i < 100; i++) {
+        list.push({
+            name: 'List Item ' + i,
+            idx: i
+        });
+    }
+    $scope.list = list;
+    $scope.imagePath = 'img/small.png';
+};
--- LocalSocial/wwwroot/app.js
+++ LocalSocial/wwwroot/app.js
@@ -7,24 +7,7 @@
 
     //SessionController.$inject = ['$scope']
 
-    app.controller('AppCtrl',
-    [
-        '$scope', '$mdSidenav', function($scope, $mdSidenav) {
-            $scope.toggleSidenav = function(menuId) {
-                $mdSidenav(menuId).toggle();
-            };
-
-            var list = [];
-            for (var i = 0; i < 100; i++) {
-                list.push({
-                    name: 'List Item ' + i,
-                    idx: i
-                });
-            }
-            $scope.list = list;
-            $scope.imagePath = 'img/small.png';
-        }
-    ]);
+    app.controller('AppCtrl', SideNavController);
     app.controller('MenuController', MenuController);
     app.controller('ContentController', ContentController);
     app.controller('SessionController', SessionController);
